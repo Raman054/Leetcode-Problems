1714ddb601deaab623b00ba3f483bc38a01ee600
Raman054 <thulasiraman291005@gmail.com>
2026-03-26 12:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (76.88%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+        for(boolean room : visited)
+        {
+            if(!room)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    boolean dfs(int room, List<List<Integer>> rooms, boolean[] visited)
+    {
+        visited[room] = true;
+        for(int key : rooms.get(room))
+        if(!visited[key])
+        {
+        {
+            dfs(key, rooms, visited);
+        }
+    }
+        return false;
+    }
+}
